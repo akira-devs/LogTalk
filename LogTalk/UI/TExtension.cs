41919edf21334be34b05c5d38a171393c70fa383
akira <akira@localdomain>
2020-05-17 23:20:33 +0900

    ローカライゼーション

---
+++ LogTalk/UI/TExtension.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace LogTalk.UI
+{
+    /// <summary>
+    /// 文字列リソースによるローカライゼーション Extension
+    /// </summary>
+    public class TExtension : MarkupExtension
+    {
+        /// <summary>
+        /// リソース名
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// コンストラクター
+        /// </summary>
+        public TExtension(string? name)
+        {
+            Name = name;
+        }
+
+        /// <summary>
+        /// 値の解決
+        /// </summary>
+        public override object? ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Properties.Resources.ResourceManager.GetString(Name) ?? Name;
+        }
+    }
+}
