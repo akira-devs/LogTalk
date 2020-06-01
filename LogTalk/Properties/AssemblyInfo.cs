f4f4fcee787ace04a49658fbbdfafe4d0c685573
akira <akira@localdomain>
2020-06-02 01:42:16 +0900

    Copyright 修正

--- LogTalk/Properties/AssemblyInfo.cs
+++ LogTalk/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LogTalk")]
-[assembly: AssemblyCopyright("Copyright © Akira 2020")]
+[assembly: AssemblyCopyright("Copyright (C) akira 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
